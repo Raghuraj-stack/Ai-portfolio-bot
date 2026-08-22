80e82d2c159613177568f0c5654202108a72278c
Raghuraj Singh Chouhan-stack <your.email@example.com>
2026-08-22 17:34:39 +0530

    allow Vercel domain in CORS

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173" , 
+                    "https://ai-portfolio-bot-navy.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
